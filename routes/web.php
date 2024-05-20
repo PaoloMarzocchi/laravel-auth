1345a77b8f35a0b0265579d304c344368d7c9abf
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-05-20 16:52:05 +0200

    add Project model with relative seeder,migration,controller and requests, add link in admin header to projects page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
+//use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,7 +32,10 @@ Route::middleware(['auth', 'verified'])
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
+
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::resource('projects', ProjectController::class);
     });
 
 require __DIR__ . '/auth.php';
